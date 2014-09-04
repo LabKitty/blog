e830388d696aeda169e8a52d869bc9d7b539239f
LabKitty <Shepard1>
2014-09-04 23:16:21 +0300

    Rename welcome controller to posts

---
+++ views/posts/posts_view.php
@@ -0,0 +1,10 @@
+<div class="span8">
+   <a><?=BASE_URL?>posts/view/<?=$post['post_id']?></a>
+        <p><?=$post["post_text"]?></p>
+        <div>
+            <span class="badge badge-success"><?=$post["post_created"]?></span><div class="pull-right"><span class="label">alice</span> <span class="label">story</span> <span class="label">blog</span> <span class="label">personal</span></div>
+        </div>
+
+ </div>
+
+
