6213fd7b4a644449e9970ba6177bef14966b45f7
LabKitty <Shepard1>
2014-09-05 00:00:29 +0300

    Rename welcome controller to posts

---
+++ views/posts/posts_view.php
@@ -0,0 +1,11 @@
+<?foreach( $posts as $post ):?>
+    <div class="span8">
+	<a href="<?=BASE_URL?>post/view/<?=$post['post_id']?>"><?=$post["post_subject"]?></a>
+        
+        <p><?=$post["post_text"]?></p>
+        <div>
+            <span class="badge badge-success"><?=$post["post_created"]?></span><div class="pull-right"><span class="label">alice</span> <span class="label">story</span> <span class="label">blog</span> <span class="label">personal</span></div>
+        </div>
+
+    </div>
+<?endforeach?>
