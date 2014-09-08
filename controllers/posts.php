a9490fe982c11b2b3c5cf99e7db1ab6c1ab8217b
LabKitty <Shepard1>
2014-09-08 14:52:17 +0300

    Add tags support

--- controllers/posts.php
+++ controllers/posts.php
@@ -4,7 +4,7 @@ class posts extends Controller{
 
 	function index(){
 		$this->posts = get_all("SELECT * FROM post");
-        $_tags = get_all("SELECT * FROM post_tags NATURAL JOIN tags");
+        $_tags = get_all("SELECT * FROM post_tags NATURAL JOIN tag");
         foreach ($_tags as $tags) {
             $this -> tags[$tag['post_id']][] = $tag['tag_name'];
         }
--- views/posts/posts_index.php
+++ views/posts/posts_index.php
@@ -1,4 +1,4 @@
-<?foreach( $posts as $post ):?>
+<?foreach ($tags[$post['post_id']] as $tag):?>
     <div class="span8">
         <h1><a href="<?= BASE_URL ?>posts/view/<?= $post['post_id'] ?>"><?=$post['post_subject']?></a></h1>
         <p><?=$post['post_text']?></p>
@@ -10,4 +10,4 @@
     </div>
 <?endforeach?>
 <?foreach ($tags[$post['post_id']] as $tag):?>
-<a href="#"><span class="label" style="background-color: #5bc0de"><?=$tag?></span></a> <?endofreach?>
+<a href="#"><span class="label" style="background-color: #5bc0de"><?=$tag?></span></a> <?endforeach?>
