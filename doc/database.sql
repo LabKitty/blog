fc0a9767d7f3889cdb7bf094840595ca8071ea02
LabKitty <Shepard1>
2014-09-15 11:28:06 +0300

    Database update login

--- doc/database.sql
+++ doc/database.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 08, 2014 at 01:39 PM
+-- Generation Time: Sep 15, 2014 at 10:27 AM
 -- Server version: 5.5.39
 -- PHP Version: 5.4.31
 
@@ -11,15 +11,34 @@ SET FOREIGN_KEY_CHECKS=0;
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
 --
 -- Database: `blog`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comment`
+--
+
+DROP TABLE IF EXISTS `comment`;
+CREATE TABLE IF NOT EXISTS `comment` (
+`comment_id` int(11) unsigned NOT NULL,
+  `comment_author` varchar(255) NOT NULL,
+  `comment_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `comment_text` text NOT NULL,
+  `post_id` int(11) unsigned NOT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+
+--
+-- Dumping data for table `comment`
+--
+
+INSERT INTO `comment` (`comment_id`, `comment_author`, `comment_created`, `comment_text`, `post_id`) VALUES
+(1, 'Lolita', '2014-09-15 07:59:18', 'Hea teada, et selline asi on olemas.', 4),
+(2, 'Meelike', '2014-09-15 07:59:18', 'Hea teada, et selline asi on olemas. :3', 5),
+(3, 'Lolita', '2014-09-15 07:59:20', 'Hea teada, et selline asi on olemas.', 4),
+(4, 'Meelike', '2014-09-15 07:59:20', 'Hea teada, et selline asi on olemas. :3', 5);
 
 -- --------------------------------------------------------
 
@@ -43,8 +62,7 @@ CREATE TABLE IF NOT EXISTS `post` (
 INSERT INTO `post` (`post_id`, `post_subject`, `post_text`, `post_created`, `user_id`) VALUES
 (4, 'Juhend', 'Vali Post tabeli Insert (Lisa) vahekaart PhpMyAdminis\r\nTäida väljad (post_id välja täitma ei pea, sest meil on AUTO_INCREMENT peal).\r\n', '2014-09-04 12:15:39', 1),
 (5, 'Anomaalia', 'onorkoodi:\r\nGuugelda bootstrap snippets (leiad ühe saidi)\r\nKasuta saidi otsingut ja otsi märksõna blog järgi postituste loendamiseks sobiv vaade ehk snippet (Simple blog lay', '2014-09-04 12:15:39', 1),
-(6, 'Juhend', 'Vali Post tabeli Insert (Lisa) vahekaart PhpMyAdminis\r\nTäida väljad (post_id välja täitma ei pea, sest meil on AUTO_INCREMENT peal).\r\n', '2014-09-04 12:15:56', 1),
-(7, 'Anomaalia', 'onorkoodi:\r\nGuugelda bootstrap snippets (leiad ühe saidi)\r\nKasuta saidi otsingut ja otsi märksõna blog järgi postituste loendamiseks sobiv vaade ehk snippet (Simple blog lay', '2014-09-04 12:15:56', 1);
+(6, 'Maalimine', 'Vali Post tabeli Insert (Lisa) vahekaart PhpMyAdminis\r\nTäida väljad (post_id välja täitma ei pea, sest meil on AUTO_INCREMENT peal).\r\n', '2014-09-04 12:15:56', 1);
 
 -- --------------------------------------------------------
 
@@ -58,6 +76,16 @@ CREATE TABLE IF NOT EXISTS `post_tags` (
   `tag_id` int(10) unsigned NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `post_tags`
+--
+
+INSERT INTO `post_tags` (`post_id`, `tag_id`) VALUES
+(4, 1),
+(4, 2),
+(5, 2),
+(6, 2);
+
 -- --------------------------------------------------------
 
 --
@@ -68,7 +96,15 @@ DROP TABLE IF EXISTS `tag`;
 CREATE TABLE IF NOT EXISTS `tag` (
 `tag_id` int(10) unsigned NOT NULL,
   `tag_name` varchar(225) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `tag`
+--
+
+INSERT INTO `tag` (`tag_id`, `tag_name`) VALUES
+(1, 'yolo'),
+(2, 'semester');
 
 -- --------------------------------------------------------
 
@@ -80,20 +116,27 @@ DROP TABLE IF EXISTS `user`;
 CREATE TABLE IF NOT EXISTS `user` (
 `user_id` int(10) unsigned NOT NULL,
   `username` varchar(25) NOT NULL,
+  `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `deleted` tinyint(3) unsigned NOT NULL DEFAULT '0'
+  `active` tinyint(3) unsigned NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`user_id`, `username`, `password`, `deleted`) VALUES
-(1, 'demo', 'demo', 0);
+INSERT INTO `user` (`user_id`, `username`, `email`, `password`, `active`) VALUES
+(1, 'demo', '', 'demo', 0);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `comment`
+--
+ALTER TABLE `comment`
+ ADD PRIMARY KEY (`comment_id`), ADD KEY `post_id` (`post_id`);
 
 --
 -- Indexes for table `post`
@@ -117,12 +160,17 @@ ALTER TABLE `tag`
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
- ADD PRIMARY KEY (`user_id`);
+ ADD PRIMARY KEY (`user_id`), ADD UNIQUE KEY `username` (`username`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
+--
+-- AUTO_INCREMENT for table `comment`
+--
+ALTER TABLE `comment`
+MODIFY `comment_id` int(11) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `post`
 --
@@ -132,7 +180,7 @@ MODIFY `post_id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
 -- AUTO_INCREMENT for table `tag`
 --
 ALTER TABLE `tag`
-MODIFY `tag_id` int(10) unsigned NOT NULL AUTO_INCREMENT;
+MODIFY `tag_id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `user`
 --
@@ -141,6 +189,12 @@ MODIFY `user_id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `comment`
+--
+ALTER TABLE `comment`
+ADD CONSTRAINT `comment_ibfk_1` FOREIGN KEY (`post_id`) REFERENCES `post` (`post_id`);
 
 --
 -- Constraints for table `post`
@@ -155,7 +209,3 @@ ALTER TABLE `post_tags`
 ADD CONSTRAINT `post_tags_ibfk_1` FOREIGN KEY (`post_id`) REFERENCES `post` (`post_id`),
 ADD CONSTRAINT `post_tags_ibfk_2` FOREIGN KEY (`tag_id`) REFERENCES `tag` (`tag_id`);
 SET FOREIGN_KEY_CHECKS=1;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- views/posts/posts_index.php
+++ views/posts/posts_index.php
@@ -20,4 +20,4 @@
 
     </div>
 
-<?endforeach?>
+>
