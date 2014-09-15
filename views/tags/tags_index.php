30b4eb5b1d2426ee3aaa793e6837a4a4c6647608
LabKitty <Shepard1>
2014-09-15 10:48:15 +0300

    Comments

--- views/tags/tags_index.php
+++ views/tags/tags_index.php
@@ -1,20 +1,5 @@
-<?foreach( $posts as $post ):?>
-<?php
-/**
- * Created by PhpStorm.
- * User: Spoink
- * Date: 5.09.14
- * Time: 13:41
- */
-
-<div class="list-group">
-  <h1><a href="<?= BASE_URL ?>posts/view/<?= $post['post_id'] ?>"><?=$post['post_subject']?></a></h1>
-Cras justo odio
-</a>
-  <a href="#" class="list-group-item">Dapibus ac facilisis in</a>
-  <a href="#" class="list-group-item">Morbi leo risus</a>
-  <a href="#" class="list-group-item">Porta ac consectetur ac</a>
-  <a href="#" class="list-group-item">Vestibulum at eros</a>
-</div>
-
-    <?endforeach?>
+<ul class="list-group">
+    <? foreach ($tags as $tag): ?>
+        <li class="list-group-item"><?=$tag['tag_name']?></li>
+    <? endforeach ?>
+</ul>
