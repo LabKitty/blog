e043c95dacb77c23b41b56329cfeef4cf677649f
LabKitty <labkittyx@gmail.com>
2014-10-21 13:02:47 +0300

    Remove log-in to user page.

--- controllers/users.php
+++ controllers/users.php
@@ -1,20 +1,14 @@
 <?php
 
-class users extends Controller
-{
-    public $requires_auth = true;
-
-    function index()
-    {
+class users extends Controller{
+    public $requires__auth = true;
+    function index(){
         $this->users = get_all("SELECT * FROM user");
 
     }
-
-    function view()
-    {
+    function view(){
         $user_id = $this->params[0];
-        $this->user = get_first("SELECT * FROM user WHERE user_id = '$user_id'");
+        $this->user = get_first("SELECT * FROM user WHERE user_id = $user_id");
 
     }
-
 }
